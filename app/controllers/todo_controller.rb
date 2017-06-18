1c60b9ed2032289377107aa5f2b6a1915dff0c0b
Karen Liu <yueyiliu123@gmail.com>
2017-06-18 22:44:49 +0000

    Delete

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,19 +8,18 @@ class TodoController < ApplicationController
     
     
     def create
-        z=Todo.new
-        z.description=params['description']
-        z.pomodoro_estimate=params['pomodoro_estimate']
-        z.save
-        redirect_to "/todo/show/#{z.id }"
+        t=Todo.new
+        t.description=params['description']
+        t.pomodoro_estimate=params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id }"
     end
     
     def show
         @todo = Todo.find_by_id (params[:id])
         @todo_description = " Make the curriculum"
         todo_id = params[:id]
-    
-    
+        
         if  todo_id =='1'
             @todo_description = "Make The Curriculum"
             @todo_promodoro_estimate = 4
@@ -37,7 +36,26 @@ class TodoController < ApplicationController
          elsif todo_id =='7'    
             @todo_description = "Have A Great Workshop"
         end
-   end     
+    end
+    
     def new
+    end
+    
+    def edit
+        @todo =Todo.find_by_id(params[:id])
+    end
+    
+    def update
+        t=Todo.find_by_id(params['id'])
+        t.description =  params['description']
+        t.pomodoro-estimate = params['pomodoro-estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+    
+    def destroy
+        t=Todo.find_by_id(params['id'])
+        t.destroy
+        redirect_to "/todo/index"
     end
 end
